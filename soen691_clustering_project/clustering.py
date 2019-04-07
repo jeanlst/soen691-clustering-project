00abda7462b176a478b1a27942cccc084aeb131e
Jean Lucas <12112853+jeanlst@users.noreply.github.com>
2019-04-07 19:12:20 -0400

    Clustering interface

---
+++ soen691_clustering_project/clustering.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Clustering 'interface'."""
+
+
+class Clustering:
+    def get_clusters(self):
+        raise NotImplementedError("The method not implemented")
+
+    def get_indexes(self):
+        raise NotImplementedError("The method not implemented")
