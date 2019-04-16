7d46f40c067bda1b40fbf6e8bf76f85be152f786
Jean Lucas <12112853+jeanlst@users.noreply.github.com>
2019-04-16 17:54:01 -0400

    Reservoir sampling

--- soen691_clustering_project/utils.py
+++ soen691_clustering_project/utils.py
@@ -21,3 +21,15 @@ def squared_euclidean_distance(x, y):
             raise ValueError('len of x ({}) != y ({})'.format(len(x), len(y)))
     else:
         raise ValueError('types of x ({}) and y ({}) are different or not supported'.format(type(x), type(y)))
+
+
+def reservoir_sampling(size):
+    i, sample = 0, []
+    while True:
+        item = yield i, sample
+        i += 1
+        k = np.random.randint(0, i)
+        if len(sample) < size:
+            sample.append(item)
+        elif k < size:
+            sample[k] = item
