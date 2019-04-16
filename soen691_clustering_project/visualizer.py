467b34a0acedd9136856feaaeb66077a7f09d2be
Jean Lucas <12112853+jeanlst@users.noreply.github.com>
2019-04-15 21:54:00 -0400

    Fixing 3d plotting import

--- soen691_clustering_project/visualizer.py
+++ soen691_clustering_project/visualizer.py
@@ -2,6 +2,7 @@
 
 """Visualizer class."""
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.gridspec import GridSpec
 from itertools import cycle
 import math
